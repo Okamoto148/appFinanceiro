e811a657612ea103109122b3acc8c7121675cbf6
Okamoto148 <85035999+Okamoto148@users.noreply.github.com>
2022-07-10 12:00:12 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,68 @@
+const styles={
+    main:{
+        width: "800px",
+        margin:"auto",
+    },
+    label:{
+        fontSize:"25px",
+    },
+    header:{
+        textAlign:"center",
+        fontSize:"30px",
+        fontWeight:"bold",
+    },
+    total:{
+        marginTop:"20px"
+    },
+    pointer:{
+        cursor:"pointer"
+    },
+    flex:{
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"spaceBetween",
+        width: "800px",
+        height:"40px",
+        borderBottom:"2px solid lightgray",
+        
+    },
+    botao:{
+        marginTop:"15px",
+        marginRight:"15px"
+    },
+    border:{
+        borderBottom:"2px solid lightgray",
+    },
+
+    borderFinal:{
+        borderBottom:"2px solid black"
+    },
+    flex2:{
+        display:"flex",
+        flexDirection:"column",
+        justifyContent:"spaceBetween",
+        width: "800px",
+        height:"auto",
+        borderBottom:"2px solid lightgray",
+    },
+    flex3:{
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"spaceBetween",
+        width: "800px",
+        height:"auto",
+        borderBottom:"2px solid lightgray",
+        
+    },
+    flex4:{
+        display:"flex",
+        flexDirection:"row",
+        justifyContent:"spaceBetween",
+        width: "800px",
+        height:"40px",
+        borderBottom:"2px solid lightgray",
+        marginTop:"20px"
+        
+    },
+
+}
